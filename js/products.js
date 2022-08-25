a3ca85a97a7cc119f6c97730611a224fe3639f78
Nicolas Silva Augustyniak <nicoaugustyniak@gmail.com>
2022-08-24 21:25:16 -0300

    ENTREGA 2 Añadido: - Funcionalidad que permite ordenar los productos de una categoria segun precio y relevancia, al igual que por precio minimo y maximo

--- js/products.js
+++ js/products.js
@@ -1,64 +1,9 @@
-//Esta funcion recibe un array con los datos de los  productos y los muestra en la pagina manipulando DOM
-function showCategoriesList(array) {
-  let htmlContentToAppend = "";
+"use stirct";
 
-  array.forEach((element) => {
-    htmlContentToAppend +=
-      // Con bootstrap podemos construir facilmente la estructura de articulos utilizando las clases correspndientes
-
-      `
-    <div class="list-group-item list-group-item-action">
-    <div class="row">
-    <div class="col-3">
-    <img src="` +
-      element.image +
-      `" alt="product image" class="img-thumbnail">
-    </div>
-    <div class="col">
-    <div class="d-flex w-100 justify-content-between">
-    <div class="mb-1">
-    <h4>` +
-      element.name +
-      " - " +
-      element.currency +
-      " " +
-      element.cost +
-      `</h4> 
-    <p> ` +
-      element.description +
-      `</p> 
-    </div>
-    <small class="text-muted">` +
-      element.soldCount +
-      ` vendidos</small> 
-    </div>
-    
-    </div>
-    </div>
-    </div>
-    `;
-    document.getElementById("products-list-container").innerHTML =
-      htmlContentToAppend;
-  });
-}
-
-//array donde se cargarán los datos recibidos:
-let productsArray = [];
-
-// Al cargar se llama a getJSONData() con la URL deseada y si el estado de esta respuesta es correcta se agregan los productos a productsArray y se llama a showCategoriesList() con el array como parametro.
-
-// Esta funcion declarada mas arriba se encargara de mostrar toda la informacion en la pagina haciendo uso del DOM
-
-document.addEventListener("DOMContentLoaded", function () {
-  getJSONData(PRODUCTS_URL).then(function (resultObj) {
-    if (resultObj.status === "ok") {
-      productsArray = resultObj.data.products;
-      showCategoriesList(productsArray);
-    }
-  });
-});
+// * ---------- CATEGORIAS ----------
 
 // Agrega el nombre de la categoria en el titulo segun corresponda
+
 switch (getId()) {
   case "101":
     catName.innerHTML = `Autos`;
@@ -88,3 +33,168 @@ switch (getId()) {
     catName.innerHTML = `Celulares`;
     break;
 }
+
+// * ---------- DECLARACIONES ----------
+
+const ORDER_ASC_BY_PRICE = "PriceASC";
+const ORDER_DESC_BY_PRICE = "PriceDESC";
+const ORDER_BY_PROD_SELL = "Sell";
+const PRODUCTS_CONTAINER = document.getElementById("products-list-container");
+const BTN_SORT_ASCEN = document.getElementById("sortAsc");
+const BTN_SORT_DESC = document.getElementById("sortDesc");
+const BTN_SORT_BY_SELL = document.getElementById("sortBySell");
+const MIN_INPUT = document.getElementById("rangeFilterCountMin");
+const MAX_INPUT = document.getElementById("rangeFilterCountMax");
+const BTN_FILTRAR = document.getElementById("rangeFilterCount");
+const BTN_LIMPIAR = document.getElementById("clearRangeFilter");
+
+let currentProductsArray = [];
+let currentSortCriteria = undefined;
+let minPrice = undefined;
+let maxPrice = undefined;
+
+// * ---------- PRODUCTOS----------
+
+// Funcion que devuelve un valor segun criterios ascendentes, descendentes o por orden de ventas (relevancia)
+function sortProducts(criteria, array) {
+  let result = [];
+  if (criteria === ORDER_ASC_BY_PRICE) {
+    result = array.sort(function (a, b) {
+      if (a.cost < b.cost) {
+        return -1;
+      }
+      if (a.cost > b.cost) {
+        return 1;
+      }
+      return 0;
+    });
+  } else if (criteria === ORDER_DESC_BY_PRICE) {
+    result = array.sort(function (a, b) {
+      if (a.cost < b.cost) {
+        return 1;
+      }
+      if (a.cost > b.cost) {
+        return -1;
+      }
+      return 0;
+    });
+  } else if (criteria === ORDER_BY_PROD_SELL) {
+    result = array.sort(function (a, b) {
+      let aCount = parseInt(a.soldCount);
+      let bCount = parseInt(b.soldCount);
+
+      if (aCount > bCount) {
+        return -1;
+      }
+      if (aCount < bCount) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+  showProductsList();
+
+  return result;
+}
+
+// Funcion que agrega al HTML la lista de productos manipulando el DOM
+const showProductsList = () => {
+  let htmlContentToAppend = "";
+
+  currentProductsArray.forEach((element) => {
+    if (
+      (minPrice == undefined ||
+        (minPrice != undefined && parseInt(element.cost) >= minPrice)) &&
+      (maxPrice == undefined ||
+        (maxPrice != undefined && parseInt(element.cost) <= maxPrice))
+    ) {
+      htmlContentToAppend +=
+        `
+        <div class="list-group-item list-group-item-action">
+        <div class="row">
+        <div class="col-3">
+        <img src="` +
+        element.image +
+        `" alt="product image" class="img-thumbnail">
+        </div>
+        <div class="col">
+        <div class="d-flex w-100 justify-content-between">
+        <div class="mb-1">
+        <h4>` +
+        element.name +
+        " - " +
+        element.currency +
+        " " +
+        element.cost +
+        `</h4>
+        <p> ` +
+        element.description +
+        `</p>
+        </div>
+        <small class="text-muted">` +
+        element.soldCount +
+        ` vendidos</small>
+        </div>
+    
+        </div>
+        </div>
+        </div>
+        `;
+    }
+    PRODUCTS_CONTAINER.innerHTML = htmlContentToAppend;
+  });
+};
+
+// Evento que se activa al cargar todos los elementos de DOM
+document.addEventListener("DOMContentLoaded", function (e) {
+  getJSONData(PRODUCTS_URL).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      currentProductsArray = resultObj.data.products;
+
+      showProductsList();
+    }
+  });
+
+  // Funciones que se activan al hacer click en los respectivos botones
+  BTN_SORT_ASCEN.onclick = () => {
+    sortProducts(ORDER_ASC_BY_PRICE, currentProductsArray);
+  };
+
+  BTN_SORT_DESC.onclick = () => {
+    sortProducts(ORDER_DESC_BY_PRICE, currentProductsArray);
+  };
+
+  BTN_SORT_BY_SELL.onclick = () => {
+    sortProducts(ORDER_BY_PROD_SELL, currentProductsArray);
+  };
+
+  BTN_LIMPIAR.onclick = () => {
+    MIN_INPUT.value = "";
+
+    MAX_INPUT.value = "";
+
+    minPrice = undefined;
+    maxPrice = undefined;
+
+    showProductsList();
+  };
+
+  BTN_FILTRAR.onclick = () => {
+    minPrice = MIN_INPUT.value;
+    maxPrice = MAX_INPUT.value;
+
+    if (minPrice != undefined && minPrice != "" && parseInt(minPrice) >= 0) {
+      minPrice = parseInt(minPrice);
+    } else {
+      minPrice = undefined;
+    }
+
+    if (maxPrice != undefined && maxPrice != "" && parseInt(maxPrice) >= 0) {
+      maxPrice = parseInt(maxPrice);
+    } else {
+      maxPrice = undefined;
+    }
+
+    showProductsList();
+  };
+});
